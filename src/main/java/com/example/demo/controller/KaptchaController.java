52c194dfd15eb30ecbde4fb6e2bf9c024b6d16f9
Jacccccc <76160295+Jacccccc@users.noreply.github.com>
2022-03-08 10:09:58 +0800

    Update KaptchaController.java

--- src/main/java/com/example/demo/controller/KaptchaController.java
+++ src/main/java/com/example/demo/controller/KaptchaController.java
@@ -119,7 +119,7 @@ public class KaptchaController {
         try {
             // 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey,此处还需注意密钥对的保密
             // 密钥可前往https://console.cloud.tencent.com/cam/capi网站进行获取
-            Credential cred = new Credential("AKIDMWQ3PUIq76oFQKxJPEm33mDNGtFJ7fwx", "lGUS9YhZ9SVGkwrVsynMefOr3xaw1oKY");
+            Credential cred = new Credential("", "");
             // 实例化一个http选项，可选的，没有特殊需求可以跳过
             HttpProfile httpProfile = new HttpProfile();
             httpProfile.setEndpoint("ses.tencentcloudapi.com");
@@ -130,7 +130,7 @@ public class KaptchaController {
             SesClient client = new SesClient(cred, "ap-hongkong", clientProfile);
             // 实例化一个请求对象,每个接口都会对应一个request对象
             SendEmailRequest req = new SendEmailRequest();
-            req.setFromEmailAddress("jac@mail.calling.fit");
+            req.setFromEmailAddress("");
             String[] destination1 = {destination};
             req.setDestination(destination1);
             Template template1 = new Template();
